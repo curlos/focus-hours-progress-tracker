8ed37ec1c2935ae4859d5053b1f77ad8cedce464
Carlos Martinez <curlosmart@gmail.com>
2024-10-22 07:01:11 -0400

    Redirect to focus hours goal page by default if no page found as the catch all route.

--- pages/_error/+Page.tsx
+++ pages/_error/+Page.tsx
@@ -1,16 +1,12 @@
-import React from 'react';
 import { usePageContext } from 'vike-react/usePageContext';
+import { redirect } from 'vike/abort';
 
 export default function Page() {
 	const { is404 } = usePageContext();
 	if (is404) {
-		return (
-			<>
-				<h1>404 Page Not Found</h1>
-				<p>This page could not be found.</p>
-			</>
-		);
+		throw redirect('/focus-hours-goal');
 	}
+
 	return (
 		<>
 			<h1>500 Internal Server Error</h1>
